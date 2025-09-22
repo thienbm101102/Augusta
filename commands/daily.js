e478a01fd958c23a8b75d8442be833210e6630a3
thienbm101102 <74740578+thienbm101102@users.noreply.github.com>
2025-09-22 15:21:16 +0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,39 +1,36 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { getUser, addBalance, getLastDaily, setLastDaily } = require('../db');
+const { getUser, addBalance, setLastDaily } = require('../db');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('diemdanh').setDescription('Nhận tiền hằng ngày (24h cooldown)'),
   async execute(interaction) {
-    const userId = interaction.user.id;
+    await interaction.deferReply();
+    const user = await getUser(interaction.user.id);
     const now = Date.now();
-    const lastDaily = await getLastDaily(userId);
+    const dailyReward = 10000;
 
-    // 24 hours in milliseconds
-    const oneDay = 24 * 60 * 60 * 1000; 
-
-    if (now - lastDaily < oneDay) {
-      const remaining = oneDay - (now - lastDaily);
+    if (now - user.lastDaily < 24 * 60 * 60 * 1000) {
+      const remaining = 24 * 60 * 60 * 1000 - (now - user.lastDaily);
       const hours = Math.floor(remaining / (1000 * 60 * 60));
       const minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
       const embed = new EmbedBuilder()
-      .setColor('Blue')
-      .setTitle('**Điểm Danh**')
-      .setDescription(`<a:AbbyHappy:1393909327848538122> **|** Bạn đã điểm danh hôm nay rồi! Thử lại sau **${hours}h ${minutes}m** nhé`)
-      .setTimestamp();
+        .setColor('Blue')
+        .setTitle('**Điểm Danh**')
+        .setDescription(`<a:AbbyHappy:1393909327848538122> **|** Bạn đã điểm danh hôm nay rồi! Thử lại sau **${hours}h ${minutes}m** nữa nhé.`)
+        .setTimestamp();
 
-      return interaction.reply({ embeds: [embed] });
+      return interaction.editReply({ embeds: [embed] });
     }
 
-    // Add balance and update last daily timestamp
-    await addBalance(userId, 10000);
-    await setLastDaily(userId, now);
+    await addBalance(interaction.user.id, dailyReward);
+    await setLastDaily(interaction.user.id, now);
 
     const embed = new EmbedBuilder()
       .setColor('Blue')
       .setTitle('**Điểm Danh**')
-      .setDescription(`<a:AbbyCheers:1393909248076943380> **|** Chúc mừng, bạn đã nhận **10000<a:diamondgem:1402590496647413811>**! Ngày mai tiếp tục quay lại nhé`)
+      .setDescription(`<a:Verified:1406631971509243974> **|** Bạn đã điểm danh thành công và nhận được **${dailyReward.toLocaleString()}**<a:diamondgem:1402590496647413811>!`)
       .setTimestamp();
 
-    return interaction.reply({ embeds: [embed] });
-  }
+    await interaction.editReply({ embeds: [embed] });
+  },
 };
