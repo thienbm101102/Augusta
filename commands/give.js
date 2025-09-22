dad163d8048974b40ad2902a247fdba83e0017b2
thienbm101102 <74740578+thienbm101102@users.noreply.github.com>
2025-09-22 17:18:18 +0700

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { addBalance, getBalance, getUser } = require('../db');
+const { getUser, addBalance, getBalance } = require('../db');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,44 +7,32 @@ module.exports = {
     .setDescription('Tặng tiền cho người khác')
     .addUserOption(opt => opt.setName('user').setDescription('Người nhận').setRequired(true))
     .addIntegerOption(opt => opt.setName('amount').setDescription('Số tiền muốn tặng').setRequired(true)),
-  
   async execute(interaction) {
     const target = interaction.options.getUser('user');
     const amount = interaction.options.getInteger('amount');
     
-    // Kiểm tra xem người dùng có cố gắng tự tặng tiền không
-    if (interaction.user.id === target.id) {
-        return interaction.reply({ content: 'Bạn không thể tự tặng tiền cho chính mình!', ephemeral: true });
+    const senderBalance = await getBalance(interaction.user.id);
+
+    if (amount <= 0 || senderBalance < amount) {
+      return interaction.reply({ content: '<a:AbbyShocked:1393909368138895411> Số <a:diamondgem:1402590496647413811> không hợp lệ!', ephemeral: true });
     }
 
-    // Kiểm tra số tiền hợp lệ
-    if (amount <= 0) {
-        return interaction.reply({ content: '<a:AbbyShocked:1393909368138895411> Số tiền không hợp lệ!', ephemeral: true });
-    }
-
-    // Lấy thông tin người gửi và người nhận từ MongoDB
-    const senderUser = await getUser(interaction.user.id);
-    const receiverUser = await getUser(target.id);
-
-    // Kiểm tra số dư người gửi
-    if (senderUser.balance < amount) {
-        return interaction.reply({ content: `<a:AbbyShocked:1393909368138895411> Bạn không đủ tiền để thực hiện giao dịch này! Bạn chỉ có **${senderUser.balance.toLocaleString()}**<a:diamondgem:1402590496647413811>.`, ephemeral: true });
-    }
-
-    // Trừ tiền người gửi và cộng tiền người nhận
     await addBalance(interaction.user.id, -amount);
     await addBalance(target.id, amount);
+
+    const senderMember = await interaction.guild.members.fetch(interaction.user.id);
+    const receiverMember = await interaction.guild.members.fetch(target.id);
+
+    const newSenderBalance = await getBalance(interaction.user.id);
 
     const embed = new EmbedBuilder()
       .setColor('Blue')
       .setTitle('**<a:Verified:1406631971509243974> Giao Dịch Thành Công**')
-      .setDescription(`**${interaction.user.tag}** đã tặng **${target.tag}** **${amount.toLocaleString()}**<a:diamondgem:1402590496647413811>`)
+      .setDescription(`<a:PenguRich:1402589007683407943> **|** **${senderMember.displayName}** đã tặng **${amount.toLocaleString()}**<a:diamondgem:1402590496647413811> cho **${receiverMember.displayName}**!`)
       .addFields(
-        { name: 'Số dư mới của bạn', value: `\`${(senderUser.balance - amount).toLocaleString()}\`<a:diamondgem:1402590496647413811>`, inline: true },
-        { name: 'Số dư mới của người nhận', value: `\`${(receiverUser.balance + amount).toLocaleString()}\`<a:diamondgem:1402590496647413811>`, inline: true }
+        { name: 'Số dư mới của bạn', value: `\`${newSenderBalance.toLocaleString()}\`<a:diamondgem:1402590496647413811>`}
       )
       .setTimestamp();
-    
-    await interaction.reply({ embeds: [embed] });
-  },
+    return interaction.reply({ embeds: [embed] });
+  }
 };
