d274e951e837bed2107b65ee5ad4083329d125f0
thienbm101102 <74740578+thienbm101102@users.noreply.github.com>
2025-09-24 01:28:32 +0700

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -40,7 +40,7 @@ const lines = await Promise.all(
 );
 
       const embed = new EmbedBuilder()
-        .setTitle('🌿 Bảng Xếp Hạng Tài Sản 🌿')
+        .setTitle('**🌿 Bảng Xếp Hạng Tài Sản**')
         .setDescription(lines.join('\n'))
         .setColor('#FFD700')
         .setThumbnail(interaction.client.user.displayAvatarURL())
