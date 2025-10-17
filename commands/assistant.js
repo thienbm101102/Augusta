73afd27ae7fbafef5f26cd6b77ebceaa2b0666b1
thienbm101102 <74740578+thienbm101102@users.noreply.github.com>
2025-10-17 17:18:07 +0700

    Update assistant.js

--- commands/assistant.js
+++ commands/assistant.js
@@ -53,7 +53,7 @@ module.exports = {
             const aiResponse = await getAIResponse(prompt);
 
             const embed = new EmbedBuilder()
-                .setTitle(`<a:Verified:1406631971509243974> **Qiuyuan Trả Lời Câu Hỏi Của Bạn**`)
+                .setTitle(`<a:Verified:1406631971509243974> **Trợ Lý Qiuyuan /「 ✦ Áp Lực Chơi Game ✦ 」**`)
                 .setDescription(`**❓ Câu hỏi của bạn:** *${prompt}*\n\n---\n\n**<a:AbbyHappy:1393909327848538122> Phản hồi:**\n${aiResponse}`)
                 .setColor('#FF5733') 
                 .setFooter({ text: `Hỏi bởi ${interaction.member.displayName}` })
@@ -71,6 +71,7 @@ module.exports = {
         }
     },
 };
+
 
 
 
