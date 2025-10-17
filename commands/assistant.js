91f9d2ec3e9f164b93425db927934bfa50f4be35
thienbm101102 <74740578+thienbm101102@users.noreply.github.com>
2025-10-18 01:39:48 +0700

    Update assistant.js

--- commands/assistant.js
+++ commands/assistant.js
@@ -79,8 +79,8 @@ module.exports = {
             const answerEmbed = new EmbedBuilder()
                 .setColor(0x0099FF)
                 .setTitle(`**<a:Verified:1406631971509243974> Trợ Lý Ảo Qiuyuan From「 ✦ Áp Lực Chơi Game ✦ 」**`)
-                .setDescription(`**<a:AbbyFlower:1393909312761364541> Câu hỏi:** ${userQuestion}\n---------------------\n**<a:AbbyHappy:1393909327848538122> Phản hồi:**\n${replyText}`)
-                .setFooter({ text: 'Phản hồi được tạo bởi Qiuyuan, có thể đúng hoặc sai nên hãy kiểm tra lại nhé !!!' });
+                .setDescription(`**Câu hỏi:** ${userQuestion}\n---------------------\n**Trả lời:**\n${replyText}`)
+                .setFooter({ text: '<a:AbbyFlower:1393909312761364541> Phản hồi được tạo bởi Qiuyuan, có thể đúng hoặc sai nên hãy kiểm tra lại nhé !!!' });
 
             await interaction.editReply({ embeds: [answerEmbed] });
 
@@ -95,6 +95,7 @@ module.exports = {
         }
     },
 }; // <-- Dấu đóng cuối cùng đảm bảo không bị lỗi SyntaxError
+
 
 
 
