d4f4addb0965f774cbea4a3d9fbbfe9c47a2aca0
thienbm101102 <74740578+thienbm101102@users.noreply.github.com>
2025-12-11 00:36:00 +0700

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -87,7 +87,7 @@ module.exports = {
     const avatarR = 60;
 
     const avatar = await Canvas.loadImage(
-      targetUser.displayAvatarURL({ extension: "png", size: 128 })
+      targetUser.displayAvatarURL({ extension: "png", size: 246 })
     );
     ctx.save();
     ctx.beginPath();
@@ -97,11 +97,11 @@ module.exports = {
     ctx.drawImage(avatar, ax - avatarR, ay - avatarR, avatarR * 2, avatarR * 2);
     ctx.restore();
 
-    const framePadding = 70;
+    const framePadding = 100;
     const framePath = path.join(__dirname, "../assets/frames", frameFile);
     if (fs.existsSync(framePath)) {
       const frame = await Canvas.loadImage(framePath);
-      const frameOffsetY = 105;
+      const frameOffsetY = 95;
       ctx.drawImage(
         frame,
         ax - avatarR - framePadding,
@@ -183,7 +183,7 @@ module.exports = {
 
     ctx.font = `14px ${FONT_FAMILY}`;
     ctx.fillStyle = "#888888";
-    ctx.fillText("© Design by neiht9073 / ✦ Đơn Giản Là Nơi Để Chơi ✦", 280, 215);
+    ctx.fillText("© Copyright © 2025 / ✦ Đơn Giản Là Nơi Để Chơi ✦", 280, 215);
 
     const attachment = new AttachmentBuilder(canvas.toBuffer("image/png"), {
       name: "taisan.png",
