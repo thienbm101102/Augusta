f37a6431bd1265e9a67ea1e80422d34ff048457c
Haruno <74740578+thienbm101102@users.noreply.github.com>
2026-09-08 20:38:39 +0700

    Update noitu.js

--- commands/noitu.js
+++ commands/noitu.js
@@ -39,7 +39,7 @@ module.exports = {
         const lastSyllable = syllables[1];
         
         const embed = new EmbedBuilder()
-            .setTitle('<a:VerifiedTwitter:1418649004912148511> ĐẤU TRƯỜNG NỐI TỪ TIẾNG VIỆT')
+            .setTitle('**<a:VerifiedTwitter:1418649004912148511> Nối Từ Tiếng Việt**')
             .setDescription(`Chủ phòng: <@${starterId}>\n\nTừ khởi đầu: 🟢 **${startWord.toUpperCase()}**\n\nNgười tiếp theo nối một từ 2 âm tiết bắt đầu bằng chữ:\n# 🎯 ${lastSyllable.toUpperCase()}\n\n*⚠️ Luật: Có nghĩa, không lặp lại, không tự nối của mình.*\n*💰 Thưởng: +100 <a:diamondgem:1418649012289933434>/từ đúng - Top 1 nhận 10000 <a:diamondgem:1418649012289933434>!*`)
             .setColor('#2ecc71')
             .setThumbnail('https://i.ibb.co/WNXqbsGT/rtx-on-wuwa.gif')
@@ -143,10 +143,10 @@ module.exports = {
                 leaderboard = '*Chưa có cao thủ nào ghi điểm.*';
             }
 
-            let endTitle = '<a:VerifiedTwitter:1418649004912148511>  HẾT GIỜ!';
+            let endTitle = '**<a:VerifiedTwitter:1418649004912148511>  Hết Giờ!**';
             let endColor = '#e74c3c';
             if (reason === 'force_stop') {
-                endTitle = '<a:VerifiedTwitter:1418649004912148511>  TRÒ CHƠI ĐÃ KẾT THÚC';
+                endTitle = '**<a:VerifiedTwitter:1418649004912148511>  Trò Chơi Đã Kết Thúc**';
                 endColor = '#95a5a6';
             }
 
