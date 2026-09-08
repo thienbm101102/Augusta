d0aced25c68b4672f2e48154beb6d7254b3dd090
Haruno <74740578+thienbm101102@users.noreply.github.com>
2026-09-08 17:40:40 +0700

    Update noitu.js

--- commands/noitu.js
+++ commands/noitu.js
@@ -43,7 +43,7 @@ module.exports = {
             .setDescription(`Chủ phòng: <@${starterId}>\n\nTừ khởi đầu: 🟢 **${startWord.toUpperCase()}**\n\nNgười tiếp theo nối một từ 2 âm tiết bắt đầu bằng chữ:\n# 🎯 ${lastSyllable.toUpperCase()}\n\n*⚠️ Luật: Có nghĩa, không lặp lại, không tự nối của mình.*\n*💰 Thưởng: +100 <a:diamondgem:1418649012289933434>/từ đúng | 💎 Top 1 nhận 10000 <a:diamondgem:1418649012289933434>!*`)
             .setColor('#2ecc71')
             .setThumbnail('https://i.ibb.co/WNXqbsGT/rtx-on-wuwa.gif')
-            .setFooter({ text: `Từ điển: ${validWords.size > 0 ? '✅' : '❌'} | Hết hạn sau 60s` });
+            .setFooter({ text: `Từ điển: ${validWords.size > 0 ? '✅' : '❌'} | Hết hạn sau 120s` });
             
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
@@ -55,8 +55,8 @@ module.exports = {
         const reply = await interaction.reply({ embeds: [embed], components: [row], fetchReply: true });
         
         const filter = m => !m.author.bot; 
-        const collector = interaction.channel.createMessageCollector({ filter, time: 60000 });
-        const btnCollector = reply.createMessageComponentCollector({ time: 60000 });
+        const collector = interaction.channel.createMessageCollector({ filter, time: 120000 });
+        const btnCollector = reply.createMessageComponentCollector({ time: 120000 });
         
         const gameState = {
             currentWord: startWord,
